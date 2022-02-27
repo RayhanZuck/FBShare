1752761ea8aa45704d9cd71e74f86000d836c44a
RayhanZuck_ <76443273+RayhanZuck@users.noreply.github.com>
2022-02-27 21:03:13 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,14 +1,5 @@
 import sys, os, subprocess, platform, struct
 
-if struct.calcsize("P")*8==64:
-  Rayhan = "rayshre.py"
-
-null=open(os.devnull, "w")
-insta= subprocess.call(["dpkg","-s","play-audio"],stdout=null,stderr=subprocess.STDOUT)
-null.close()
-if insta !=0:
-  os.system('pkg install play-audio -y &> /dev/null')
-
 while True :
   try :
     import pwinput, requests, bs4, beautifultable, termcolor
@@ -17,4 +8,4 @@ while True :
   else:
     break
 
-os.system(f'chmod 777 python {Rayhan}')
+os.system('python rayshre.py')
