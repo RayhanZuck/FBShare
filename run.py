fedb1cf5fcb74edb55e02844103b2fc333ba9df4
RayhanZuck_ <76443273+RayhanZuck@users.noreply.github.com>
2022-03-10 09:59:18 +0700

    Add files via upload

---
+++ run.py
@@ -0,0 +1,7 @@
+import sys, os
+
+if __name__ == '__main__':
+    os.system("git pull")
+    os.system("python SBoHwkZamuTizWppFxzHgophhPkcYzhGeOBxfQJUcQIFWKCtDdpSNLyqjrgfbBgaoRTxxwwbckmpTcoFZhZHXUUYNjSUHAHStVheIAXbEPNJfHcycwCISsjKxwigdVSWrMVtdzJUQwsMxmuLgukvWpfTVyvQvqDfyuHvRckQmKRaKWEMJJJbpcODdOxLUdPYxmgOheDY.so")
+    else:
+    exit(f"keren bang")
