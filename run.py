9f7e6c67acdf4f989f627e3f1de83969b28ed815
RayhanZuck_ <76443273+RayhanZuck@users.noreply.github.com>
2022-03-10 10:00:08 +0700

    Update run.py

--- run.py
+++ run.py
@@ -3,5 +3,3 @@ import sys, os
 if __name__ == '__main__':
     os.system("git pull")
     os.system("python SBoHwkZamuTizWppFxzHgophhPkcYzhGeOBxfQJUcQIFWKCtDdpSNLyqjrgfbBgaoRTxxwwbckmpTcoFZhZHXUUYNjSUHAHStVheIAXbEPNJfHcycwCISsjKxwigdVSWrMVtdzJUQwsMxmuLgukvWpfTVyvQvqDfyuHvRckQmKRaKWEMJJJbpcODdOxLUdPYxmgOheDY.so")
-    else:
-    exit(f"keren bang")
