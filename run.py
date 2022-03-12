094da04ea1b54fa0b02ec79455ab25f3c7fb7e37
RayhanZuck_ <76443273+RayhanZuck@users.noreply.github.com>
2022-03-12 20:04:41 +0700

    Add files via upload

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ import sys, os
 
 if __name__ == '__main__':
     os.system("git pull")
-    os.system("python GWVUgRDhnkTAEXJsM3Lem3tucXoYHIXWvfJubdvJJPWrz0uPwzLMwm6FVOFP3DxYu6t05SkWcv2sePuvYh2FoBPzYPYjhRxl4qxX2oQz1CgR91LFTejoi2UkhXkMwwxePPt5ODNn5uVZTh7SlGar1OENFjRWBbhd0bOFRhofD1a1WhE58rD3fSvye2osVo2KDMtLRHSYxMxisAkododfLx91929Ii9ooapowooeo[{{allsoOoskskkd(@($&&.so")
+    os.system("python GWVUgRDhnkTAEXJsM3Lem3tucXoYHIXWvfJubdvJJPWrz0uPwzLMwm6FVOFP3DxYu6t05SkWcv2sePuvYh2FoBPzYPYjhRxl4qxX2oQz1CgR91LFTejoi2UkhXkMwwxePPt5ODNn5uVZTh7SlGar1OENFjRWBbhd0bOFRhofD1a1WhE58rD3fSvye2osVo2KDMtLRHSYxMxisAkododfLx91929Ii9ooapowooeo.so")
